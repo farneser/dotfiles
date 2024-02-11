651dc009a2f9b014d96f137352879c4d7454da78
farneser <farneser1@gmail.com>
2024-02-11 04:04:46 +0300

    chore: hyprland binds for asus

---
+++ config/hypr/scripts/brightness_kb.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Get keyboard brightness
+get_kbd_backlight() {
+	echo $(brightnessctl -d '*::kbd_backlight' -m | cut -d, -f4)
+}
+
+# Notify
+notify_user() {
+	notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "Keyboard Brightness : $current%"
+}
+
+# Change brightness
+change_kbd_backlight() {
+	brightnessctl -d *::kbd_backlight set "$1" && get_icon && notify_user
+}
+
+# Execute accordingly
+case "$1" in
+	"--get")
+		get_kbd_backlight
+		;;
+	"--inc")
+		change_kbd_backlight "+30%"
+		;;
+	"--dec")
+		change_kbd_backlight "30%-"
+		;;
+	*)
+		get_kbd_backlight
+		;;
+esac
